d989d16ddf2afa73492d12723ed25e6372a233c0
Techy <brandynabel@gmail.com>
2016-03-23 19:36:13 -0400

    Added BankItems Class to Customs Namespace. Adds Items to new players Banks.

---
+++ devel/Scripts/Customs/BankItems.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Server;
+using Server.Items;
+
+namespace Customs
+{
+    class BankItems
+    {
+        public static void Initialize()
+        {
+            EventSink.CharacterCreated += new CharacterCreatedEventHandler(NewPlayerItems);
+        }
+        
+        public static void NewPlayerItems(CharacterCreatedEventArgs e)
+        {
+            Mobile m_mobile = e.Mobile;
+            m_mobile.BankBox.DropItem(new BankCheck(5000));
+            m_mobile.BankBox.DropItem(new BagOfAllReagents(10));
+        }
+
+    }
+}
