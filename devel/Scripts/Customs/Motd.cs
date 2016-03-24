a6c78505de1284e539402a0544a9769003803413
Techy <brandynabel@gmail.com>
2016-03-23 20:41:17 -0400

    Added Staff MOTD Staff default open StaffMOTD Staff can force player motd to open by using [motd player

--- devel/Scripts/Customs/Motd.cs
+++ devel/Scripts/Customs/Motd.cs
@@ -11,6 +11,8 @@ namespace Server.Gumps
 {
 	public class MotdGump : Gump
 	{
+
+        private const string StaffMotdPath = @"Data/staffmotd.txt";
         private const string MotdPath = @"Data/motd.txt";
 
         public static void Initialize()
@@ -19,7 +21,7 @@ namespace Server.Gumps
             EventSink.Login += new LoginEventHandler(Motd_OnConnected);
         }
 
-		public MotdGump()
+		public MotdGump(AccessLevel Access, Boolean OverrideAccess)
 			: base( 0, 0 )
 		{
 			this.Closable=true;
@@ -28,15 +30,15 @@ namespace Server.Gumps
 			this.Resizable=false;
 			this.AddPage(0);
 			this.AddBackground(0, 0, 375, 225, 9200);
-			this.AddLabel(9, 13, 0, @"Welcome to TechUO!");
-			this.AddCheck(12, 202, 210, 211, false, 0);
-			this.AddLabel(37, 202, 0, @"Don't show me this again.");
+            this.AddLabel(9, 13, 0, @"Welcome to TechUO!");
             this.AddButton(140, 10, (int)0x2A58, (int)0X2A44, (int)Buttons.Rules, GumpButtonType.Reply, 0); //Rules
             this.AddLabel(180, 14, 2049, @"Rules");
             this.AddButton(260, 10, (int)0x2A30, (int)0X2A44, (int)Buttons.Changelog, GumpButtonType.Reply, 0); //Chanelog
             this.AddLabel(290, 14, 2049, @"Changelog");
+			this.AddCheck(12, 202, 210, 211, false, 0);
+			this.AddLabel(37, 202, 0, @"Don't show me this again.");
 			this.AddButton(300, 195, 247, 248, (int)Buttons.Close, GumpButtonType.Reply, 0); //Close
-            this.AddHtml(10, 40, 355, 146, GetMotdMessage(), (bool)true, (bool)true);
+            this.AddHtml(10, 40, 355, 146, GetMotdMessage(Access, OverrideAccess), (bool)true, (bool)true);
 		}
 		
 		public enum Buttons
@@ -47,15 +49,31 @@ namespace Server.Gumps
 			Close,
 		}
 
-        public string GetMotdMessage()
+        public string GetMotdMessage(AccessLevel Access, Boolean OverrideStaff)
         {
-            try
+            if (Access > AccessLevel.Player && !OverrideStaff)
             {
-                return File.ReadAllText(MotdPath);
+                try
+                {
+                    return File.ReadAllText(StaffMotdPath);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(string.Concat("Error retrieving Staff Motd : ", ex.Message));
+                }
             }
-            catch 
+            else
             {
+                try
+                {
+                    return File.ReadAllText(MotdPath);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(string.Concat("Error retrieving Player Motd : ", ex.Message));
+                }
             }
+           
               
 
             return @"";
@@ -99,13 +117,26 @@ namespace Server.Gumps
 
         public static void Motd_OnCommand(CommandEventArgs e)
         {
-            e.Mobile.SendGump(new MotdGump());
+            if (e.GetString(0) == "player")
+            {
+                e.Mobile.SendMessage(e.GetString(0));
+                e.Mobile.SendMessage(e.GetString(0).Contains("player").ToString());
+                e.Mobile.SendMessage("YES");
+                e.Mobile.SendGump(new MotdGump(e.Mobile.AccessLevel, true));
+            }
+            else
+            {
+                e.Mobile.SendMessage(e.GetString(0));
+                e.Mobile.SendMessage(e.GetString(0).Contains("player").ToString());
+                e.Mobile.SendMessage("NO");
+                e.Mobile.SendGump(new MotdGump(e.Mobile.AccessLevel, false));
+            }
         }
 
         public static void Motd_OnConnected(LoginEventArgs e)
         {
             if (e.Mobile.ShowMotd)
-            e.Mobile.SendGump(new MotdGump());
+            e.Mobile.SendGump(new MotdGump(e.Mobile.AccessLevel, false));
         }
 
         private static void ToggleShowAgain(Mobile from)
